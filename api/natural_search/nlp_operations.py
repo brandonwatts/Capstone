60829aed85898e0e01531a1ea9defb0b5ebacae2
Brandon Watts <brandonwatts@outlook.com>
2017-11-13 17:14:50 -0500

    Filepath fix

--- api/natural_search/nlp_operations.py
+++ api/natural_search/nlp_operations.py
@@ -14,8 +14,8 @@ __license__ = 'MIT'
 __version__ = '0.1'
 
 st = StanfordNERTagger(
-    '/Users/brandonwatts/Desktop/Capstone/stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz',
-    '/Users/brandonwatts/Desktop/Capstone/stanford-ner-2017-06-09/stanford-ner.jar',
+    'stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz',
+    'stanford-ner-2017-06-09/stanford-ner.jar',
     encoding='utf-8')
 
 us_states = [
