7bf695daa2ab4bb8b843ef53cf1d97e467aa0476
Brandon Watts <brandonwatts@outlook.com>
2017-11-14 13:25:09 -0500

    Add back setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Capstone',
+    version='1.0',
+    packages=['flask-restplus','marshmallow','nltk'],
+    url='',
+    license='MIT',
+    author='',
+    author_email='',
+    description=''
+)
