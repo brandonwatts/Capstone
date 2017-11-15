ec28f744cd0828cd8916f20e62968b3893c48d14
Brandon Watts <brandonwatts@outlook.com>
2017-11-15 09:45:42 -0500

    Extraction using spacy

--- api/natural_search/endpoints/nlp.py
+++ api/natural_search/endpoints/nlp.py
@@ -12,11 +12,10 @@ __license__ = 'MIT'
 __version__ = '0.1'
 
 ns = api.namespace('nlp', description='Operations related to Natural Language Processing')
-log = logging.getLogger(__name__)           # Logger
-query_arguments = reqparse.RequestParser()  # Arguements
+log = logging.getLogger(__name__)
+query_arguments = reqparse.RequestParser()
 
-query_arguments.add_argument('request', type=str, required=True)    # We are expecting an arguement called request that
-                                                                    # is a string. (WILL EVENTLY TAKE A JSON OBJECT)
+query_arguments.add_argument('request', type=str, required=True)
 
 
 @ns.route('/')
--- api/natural_search/nlp_operations.py
+++ api/natural_search/nlp_operations.py
@@ -1,22 +1,9 @@
 import re
-
-from nltk import word_tokenize
-from nltk.tag import StanfordNERTagger
-
+import spacy
 from api.natural_search.Models.ApiResponse import ApiResponse
 from api.natural_search.Models.Schemas.ApiSchema import ApiSchema
 
-'''nlp_operations.py contains all the operations that are used to transform a request to an CoStar API request '''
-
-__author__ = "Brandon Watts"
-__credits__ = ['Jonathan Cary', 'Austin Green']
-__license__ = 'MIT'
-__version__ = '0.1'
-
-st = StanfordNERTagger(
-    'stanford-ner-2017-06-09/classifiers/english.all.3class.distsim.crf.ser.gz',
-    'stanford-ner-2017-06-09/stanford-ner.jar',
-    encoding='utf-8')
+nlp = spacy.load("en")
 
 us_states = [
     'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
@@ -34,37 +21,23 @@ us_states = [
 
 
 def response(request):
-    states = extract_states(request)
-    city = extract_city(request)
+    doc = nlp(request)
+    states = extract_states(doc.ents)
+    city = extract_city(doc.ents)
     zip_code = extract_zip(request)
     api_response = ApiResponse(states=states, city=city, zip_code=zip_code)
     schema = ApiSchema()
     return schema.dump(api_response)
 
 
-def city_or_state(location):
-    if location in us_states:
-        return "State"
-    else:
-        return "City"
+def extract_states(ents):
+    return list(filter(lambda x: x in us_states, map(lambda x: str(x), ents)))
 
 
-def extract_states(text):
-    return map(lambda x: x[0], filter(lambda x: x[1] == "State", extract_locations(text)))
-
-
-def extract_city(text):
-    return map(lambda x: x[0], filter(lambda x: x[1] == "City", extract_locations(text)))
+def extract_city(ents):
+    return list(filter(lambda x: x not in us_states, map(lambda x: str(x), ents)))
 
 
 def extract_zip(text):
     zip_regex = re.compile('\\b\d{5}\\b')
     return zip_regex.findall(text)
-
-
-def extract_locations(text):
-    tokenized_text = word_tokenize(text)
-    classified_text = st.tag(tokenized_text)
-    locations = map(lambda x: (x, city_or_state(x)),
-                    map(lambda x: x[0], filter(lambda x: x[1] == "LOCATION", classified_text)))
-    return locations
