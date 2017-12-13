e3b38539ac8d5f3b7fc3434b3b8c62d20cca65b1
Brandon Watts <brandonwatts@outlook.com>
2017-12-13 17:10:26 -0500

    cleanup

--- api/natural_search/Models/ApiResponse.py
+++ api/natural_search/Models/ApiResponse.py
@@ -1,23 +1,7 @@
 """APIResponse.py is the response the the api/nlp endpoint"""
 
-__author__ = "Brandon Watts"
-__credits__ = ['Jonathan Cary', 'Austin Green']
-__license__ = 'MIT'
-__version__ = '0.1'
-
 
 class ApiResponse(object):
-    def __init__(self, state, city, zip_code, min_sqft, max_sqft, min_price, max_price, min_bed, max_bed, pricing_type,
-                 address, build_year):
-        self.state = state
-        self.city = city
-        self.zip_code = zip_code
-        self.min_sqft = min_sqft
-        self.max_sqft = max_sqft
-        self.min_price = min_price
-        self.max_price = max_price
-        self.min_bed = min_bed,
-        self.max_bed = max_bed,
-        self.pricing_type = pricing_type
-        self.address = address
-        self.build_year = build_year
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
