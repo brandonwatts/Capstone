32332cfa80125b06e27fbea4117a1d650f28f388
Brandon Watts <brandonwatts@outlook.com>
2018-02-15 18:01:07 -0500

    More cleanup

--- Api/NLP.py
+++ Api/NLP.py
@@ -14,7 +14,7 @@ class NLP:
     def __init__(self):
         self.extractions = {}
         
-    def _addExtraction(self, name, value):
+    def _add_extraction(self, name, value):
         self.extractions[name] = value
         
     def parse(self, request):
@@ -25,13 +25,13 @@ class NLP:
         matcher.add('SearchRadius', self.on_search_radius_match, search_radius_pattern)
         doc = nlp(request)
         matcher(doc)
-        self.define_extraction_points(doc)
+        self._define_extraction_points(doc)
         return self.extractions
     
     def on_search_radius_match(self, matcher, doc, id, matches):
         match_id, start, end = matches[id]
         searchRadius = doc[start:end]
-        self._addExtraction("search_radius", searchRadius)
+        self._add_extraction("search_radius", searchRadius)
     
     def on_star_rating_match(self, matcher, doc, id, matches):
         star_ratings = []
@@ -39,62 +39,57 @@ class NLP:
             match_id, start, end = match
             starRating = doc[start:end]
             star_ratings.append(starRating.text)
-        self._addExtraction("star_rating", star_ratings)
+        self._add_extraction("star_rating", star_ratings)
     
-    def define_extraction_points(self, doc):
-        self._addExtraction("state", self.extract_state(doc))
-        self._addExtraction("city", self.extract_city(doc))
-        self._addExtraction("zip_code", self.extract_zip_code(doc))
-        self._addExtraction("min_sqft", self.extract_min_sqft(doc))
-        self._addExtraction("max_sqft", self.extract_max_sqft(doc))
-        self._addExtraction("min_price", self.extract_min_price(doc))
-        self._addExtraction("max_price", self.extract_max_price(doc))
-        self._addExtraction("min_bed", self.extract_min_bed(doc))
-        self._addExtraction("max_bed", self.extract_max_bed(doc))
-        self._addExtraction("pricing_type", self.extract_pricing_type(doc))
-        self._addExtraction("address", self.extract_address(doc))
-        self._addExtraction("build_year", self.extract_build_year(doc))
-        self._addExtraction("dog_friendly", self.extract_dog_friendly(doc))
-        self._addExtraction("cat_friendly", self.extract_cat_friendly(doc))
-        self._addExtraction("has_pool", self.extract_has_pool(doc))
-        self._addExtraction("has_elevator", self.extract_has_elevator(doc))
-        self._addExtraction("has_fitness_center", self.extract_has_fitness_center(doc))
-        self._addExtraction("has_wheelchair_access", self.extract_has_wheelchair_access(doc))
-        self._addExtraction("has_dishwasher", self.extract_has_dishwasher(doc))
-        self._addExtraction("has_air_conditioning", self.extract_has_air_conditioning(doc))
-        self._addExtraction("has_parking", self.extract_has_parking(doc))
-        self._addExtraction("is_furnished", self.extract_is_furnished(doc))
-        self._addExtraction("has_laundry_facilities", self.extract_has_laundry_facilities(doc))
-        self._addExtraction("property_type", self.extract_property_type(doc))
+    def _define_extraction_points(self, doc):
+        self._add_extraction("state", self.extract_state(doc))
+        self._add_extraction("city", self.extract_city(doc))
+        self._add_extraction("zip_code", self.extract_zip_code(doc))
+        self._add_extraction("min_sqft", self.extract_min_sqft(doc))
+        self._add_extraction("max_sqft", self.extract_max_sqft(doc))
+        self._add_extraction("min_price", self.extract_min_price(doc))
+        self._add_extraction("max_price", self.extract_max_price(doc))
+        self._add_extraction("min_bed", self.extract_min_bed(doc))
+        self._add_extraction("max_bed", self.extract_max_bed(doc))
+        self._add_extraction("pricing_type", self.extract_pricing_type(doc))
+        self._add_extraction("address", self.extract_address(doc))
+        self._add_extraction("build_year", self.extract_build_year(doc))
+        self._add_extraction("dog_friendly", self.extract_dog_friendly(doc))
+        self._add_extraction("cat_friendly", self.extract_cat_friendly(doc))
+        self._add_extraction("has_pool", self.extract_has_pool(doc))
+        self._add_extraction("has_elevator", self.extract_has_elevator(doc))
+        self._add_extraction("has_fitness_center", self.extract_has_fitness_center(doc))
+        self._add_extraction("has_wheelchair_access", self.extract_has_wheelchair_access(doc))
+        self._add_extraction("has_dishwasher", self.extract_has_dishwasher(doc))
+        self._add_extraction("has_air_conditioning", self.extract_has_air_conditioning(doc))
+        self._add_extraction("has_parking", self.extract_has_parking(doc))
+        self._add_extraction("is_furnished", self.extract_is_furnished(doc))
+        self._add_extraction("has_laundry_facilities", self.extract_has_laundry_facilities(doc))
+        self._add_extraction("property_type", self.extract_property_type(doc))
     
     def extract_state(self, doc):
         for entity in doc.ents:
             if entity.label_ == 'GPE' and entity.lemma_.upper() in us_states:
                 state = self.Utils.abbreviate(entity.lemma_)
-        return state
+                return state
 
     def extract_city(self, doc):
-        return "Richmond"
-        #return [ent.lemma_.upper() for ent in doc.ents if ent.label_ == 'GPE' and ent.text.upper() not in us_states]
+        for entity in doc.ents:
+            if entity.label_ == 'GPE' and entity.lemma_.upper() not in us_states:
+                return entity.lemma_.title()
     
     def extract_zip_code(self, doc):
         return [token.text for token in doc if
                 re.match('\d{5}', token.text) and list(token.children) == []]
     
-    def is_sqft(self, token):
-        tags = ('FEET', 'FOOT', 'SQUAREFEET', 'SQUAREFOOT',
-                'SQUARE', 'SQFT', 'SQ', 'FT')
-        return token.pos_ == "NUM" and token.head.lemma_.upper() in tags
-    
     def extract_min_sqft(self, doc):
         return [token.text for token in doc
-                if self.is_sqft(token) and not self.Utils.is_max_quantity(token)]
+                if self.Utils.is_sqft(token) and not self.Utils.is_max_quantity(token)]
     
     def extract_max_sqft(self, doc):
         return [token.text for token in doc
-                if self.is_sqft(token) and self.Utils.is_max_quantity(token)]
+                if self.Utils.is_sqft(token) and self.Utils.is_max_quantity(token)]
 
-    
     def extract_min_price(self, doc):
         return [token.text
                 for token in doc if self.Utils.is_price(token) and not self.Utils.is_max_quantity(token)]
@@ -102,33 +97,23 @@ class NLP:
     def extract_max_price(self, doc):
         return [token.text
                 for token in doc if self.Utils.is_price(token) and self.Utils.is_max_quantity(token)]
-    
-    def find_pricing_type(self, token):
-        for child in token.children:
-            if child.text.upper() == "PER":
-                for pricing in child.children:
-                    return pricing
-    
+
     def extract_pricing_type(self, doc):
         types = []
         for token in doc:
             if self.Utils.is_price(token):
-                result = self.find_pricing_type(token)
+                result = self.Utils.find_pricing_type(token)
                 if result:
                     types.append(result.text)
         return types
     
-    def is_bed(self, token):
-        tags = ('BED', 'ROOM', 'BEDROOM', 'PEOPLE')
-        return token.pos_ == "NUM" and token.head.lemma_.upper() in tags
-    
     def extract_min_bed(self, doc):
         return [token.text for token in doc
-                if self.is_bed(token) and not self.Utils.is_max_quantity(token)]
+                if self.Utils.is_bed(token) and not self.Utils.is_max_quantity(token)]
     
     def extract_max_bed(self, doc):
         return [token.text for token in doc
-                if self.is_bed(token) and self.Utils.is_max_quantity(token)]
+                if self.Utils.is_bed(token) and self.Utils.is_max_quantity(token)]
     
     def extract_address(self, doc):
         return [token.text for token in doc if token.ent_type_ == "FAC"]
@@ -213,5 +198,22 @@ class NLP:
 
         @staticmethod
         def abbreviate(state):
-            print("abb", us.states.lookup(state).abbr)
             return us.states.lookup(state).abbr
+
+        @staticmethod
+        def is_sqft(token):
+            tags = ('FEET', 'FOOT', 'SQUAREFEET', 'SQUAREFOOT',
+                    'SQUARE', 'SQFT', 'SQ', 'FT')
+            return token.pos_ == "NUM" and token.head.lemma_.upper() in tags
+
+        @staticmethod
+        def is_bed(token):
+            tags = ('BED', 'ROOM', 'BEDROOM', 'PEOPLE')
+            return token.pos_ == "NUM" and token.head.lemma_.upper() in tags
+
+        @staticmethod
+        def find_pricing_type(token):
+            for child in token.children:
+                if child.text.upper() == "PER":
+                    for pricing in child.children:
+                        return pricing
