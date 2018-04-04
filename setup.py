22282b466adb05bccfd562ff6c2dc02c2b9d5ad6
Jonathan Cary <jonathan@debian.local>
2018-04-04 14:58:58 -0400

    Removed nltk from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='Capstone',
     version='1.0',
     packages=['smartsearch'],
-    install_requires=['aiohttp', 'flask-restplus', 'marshmallow', 'nltk', 'spacy', 'us'],
+    install_requires=['aiohttp', 'flask-restplus', 'marshmallow', 'spacy', 'us'],
     url='',
     license='MIT',
     author='',
