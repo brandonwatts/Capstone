f89a972dd47e5228b569d9b063aae0629a1b56ff
greenmachine757 <mrgreennorris@gmail.com>
2018-04-11 07:08:48 -0400

    Integration tests
    .

--- smartsearch/tests/integration.py
+++ smartsearch/tests/integration.py
@@ -9,11 +9,66 @@ class TestRequestTypeGeneral(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    def testSimpleNlpResponse(self):
-        result = self.app.get('http://localhost:5000/nlp?request=%22Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia%22&request_type=General')
+    def testCityAndStateWithPeriodResponse(self):
+        result = self.app.get('http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia.&request_type=General')
         data = json.loads(result.data)
         self.assertEqual('Richmond', data.get('city'))
         self.assertEqual('VA', data.get('state'))
+
+    def testCityAndStateWithoutPeriodResponse(self):
+        result = self.app.get('http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia&request_type=General')
+        data = json.loads(result.data)
+        self.assertEqual('Richmond', data.get('city'))
+        self.assertEqual('VA', data.get('state'))
+
+    def testCityAndStateWithCommaResponse(self):
+        result = self.app.get(
+            'http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia.&request_type=General')
+        data = json.loads(result.data)
+        self.assertEqual('Richmond', data.get('city'))
+        self.assertEqual('VA', data.get('state'))
+
+    def testCityAndStateWithoutCommaResponse(self):
+        result = self.app.get(
+            'http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%20Virginia.&request_type=General')
+        data = json.loads(result.data)
+        self.assertEqual('Richmond', data.get('city'))
+        self.assertEqual('VA', data.get('state'))
+
+    def testCityStateAndZipWithPeriodResponse(self):
+        result = self.app.get('http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia%2023221.&request_type=General')
+        data = json.loads(result.data)
+        self.assertEqual('Richmond', data.get('city'))
+        self.assertEqual('VA', data.get('state'))
+        self.assertEqual('23221', data.get('zip_code'))
+
+    def testCityStateAndZipWithoutPeriodResponse(self):
+        result = self.app.get('http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia%2023221&request_type=General')
+        data = json.loads(result.data)
+        self.assertEqual('Richmond', data.get('city'))
+        self.assertEqual('VA', data.get('state'))
+        self.assertEqual('23221', data.get('zip_code'))
+
+    def tearDown(self):
+        pass
+
+class TestRequestTypeApartments(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def testRichmondVirginiaWithPeriodResponse(self):
+        result = self.app.get(
+            'http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia.&request_type=Apartments')
+        data = json.loads(result.data)
+        self.assertTrue(data.__len__ > 10)
+
+    def testRichmondVirginiaWithoutPeriodResponse(self):
+        result = self.app.get(
+            'http://localhost:5000/nlp?request=Show%20me%20all%20apartments%20in%20Richmond%2C%20Virginia&request_type=Apartments')
+        data = json.loads(result.data)
+        self.assertTrue(data.__len__ > 10)
 
     def tearDown(self):
         pass
