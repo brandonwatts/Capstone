3af59d9fb3fe799d2f96d5a574665bb96859b432
Brandon Watts <brandonwatts@outlook.com>
2018-04-11 14:17:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='Capstone',
+    name='smartsearch',
     version='1.0',
     packages=['smartsearch'],
-    install_requires=['aiohttp', 'flask-restplus', 'marshmallow', 'spacy', 'us'],
-    url='',
+    install_requires=[
+        'Flask==0.12.2',
+        'flask-restplus==0.10.1',
+        'marshmallow==2.14.0',
+        'us==1.0.0',
+        'spacy==2.0.6'
+    ],
+    package_dir={'': 'smartsearch'},
+    url='https://github.com/joshsticks/Capstone',
     license='MIT',
-    author='',
-    author_email='',
-    description=''
+    author='Jonathan Cary, Austin Green, Brandon Watts',
+    author_email='caryjb@mymail.vcu.edu, greenaa2@mymail.vcu.edu, wattsbc2@mymail.vcu.edu',
+    description='Natural Processing API allowing for Natural Language Search'
 )
