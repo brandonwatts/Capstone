8acf4a10979ed2e43efe9380cf02bae7f9606022
Jonathan Cary <jonathan@debian.local>
2018-04-12 04:51:37 -0400

    Initial sentence negation

--- smartsearch/nlp.py
+++ smartsearch/nlp.py
@@ -28,7 +28,60 @@ def parse(text):
             span.merge()
         except IndexError:
             pass
-
+    
     field_matcher(doc)
     extract_references(doc, extractions)
+    
+    if is_negated(doc):
+        negate(extractions)
+    
     return extractions
+
+def is_negated(doc):
+    token = doc[0]
+    while token != token.head:
+        token = token.head
+    
+    children = [i.text.lower() for i in token.children]
+    negations = ["no", "not", "n't", "nothing", "nowhere"]
+    
+    if any(i in children for i in negations):
+        return True
+    
+    # this could be improved using what's in references
+    topics = ["house", "apartment", "building", "anywhere", "place"]
+    
+    for i in token.children:
+        if i.text.lower() in topics:
+            grandchildren = [j.text.lower() for j in i.children]
+            if any(j in grandchildren for j in negations):
+                return True
+    
+    return False
+
+def negate(extractions):
+    if extractions.get("max_price") and not extractions.get("min_price"):
+        extractions["min_price"] = extractions["max_price"]
+        extractions["max_price"] = None
+    if extractions.get("min_price") and not extractions.get("max_price"):
+        extractions["max_price"] = extractions["min_price"]
+        extractions["min_price"] = None
+
+    if extractions.get("max_sqft") and not extractions.get("min_sqft"):
+        extractions["min_sqft"] = extractions["max_sqft"]
+        extractions["max_sqft"] = None
+    if extractions.get("min_sqft") and not extractions.get("max_sqft"):
+        extractions["max_sqft"] = extractions["min_sqft"]
+        extractions["min_sqft"] = None
+        
+    if extractions.get("max_bed") and not extractions.get("min_bed"):
+        extractions["min_bed"] = extractions["max_bed"]
+        extractions["max_bed"] = None
+    if extractions.get("min_bed") and not extractions.get("max_bed"):
+        extractions["max_bed"] = extractions["min_bed"]
+        extractions["min_bed"] = None
+    
+    if extractions.get("dog_friendly"):
+        extractions.get["dog_friendly"] = False
+    if extractions.get("cat_friendly"):
+        extractions.get["cat_friendly"] = False
