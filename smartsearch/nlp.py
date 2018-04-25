8a9931b750c74ddd639e803faee0fc80d50aa41b
Brandon Watts <brandonwatts@outlook.com>
2018-04-25 15:04:14 -0400

    Fix word vectors and add comments

--- smartsearch/nlp.py
+++ smartsearch/nlp.py
@@ -10,6 +10,7 @@ def static_args(**kwargs):
     NEEDS DOC
     # TODO
     """
+
     def decorate(func):
         for k in kwargs:
             setattr(func, k, kwargs[k])
@@ -59,13 +60,13 @@ def parse(text):
             span.merge()
         except IndexError:
             pass
-    
+
     field_matcher(doc)
     extract_references(doc, extractions)
-    
+
     if is_negated(doc):
         negate(extractions)
-    
+
     return extractions
 
 
@@ -85,22 +86,22 @@ def is_negated(doc):
     token = doc[0]
     while token != token.head:
         token = token.head
-    
+
     children = [i.text.lower() for i in token.children]
     negations = ["no", "not", "n't", "nothing", "nowhere"]
-    
+
     if any(i in children for i in negations):
         return True
-    
+
     # this could be improved using what's in references
     topics = ["house", "apartment", "building", "anywhere", "place"]
-    
+
     for i in token.children:
         if i.text.lower() in topics:
             grandchildren = [j.text.lower() for j in i.children]
             if any(j in grandchildren for j in negations):
                 return True
-    
+
     return False
 
 
@@ -122,16 +123,15 @@ def negate(extractions):
     if extractions.get("min_sqft") and not extractions.get("max_sqft"):
         extractions["max_sqft"] = extractions["min_sqft"]
         extractions["min_sqft"] = None
-        
+
     if extractions.get("max_bed") and not extractions.get("min_bed"):
         extractions["min_bed"] = extractions["max_bed"]
         extractions["max_bed"] = None
     if extractions.get("min_bed") and not extractions.get("max_bed"):
         extractions["max_bed"] = extractions["min_bed"]
         extractions["min_bed"] = None
-    
+
     if extractions.get("dog_friendly"):
         extractions.get["dog_friendly"] = False
     if extractions.get("cat_friendly"):
         extractions.get["cat_friendly"] = False
-
